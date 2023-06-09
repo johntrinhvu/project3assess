76f2ba8a2db8e649440afd792de07e8d9bc65cd9
John Vu <johntrinhvu@yahoo.com>
2023-06-09 12:47:27 -0700

    complete 3.2

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import WishList
 
 # Create your views here.
+def index(request):
+    wish_items = WishList.objects.all()
+    return render(request, 'index.html', {
+        'wish_items': wish_items
+    })
+
+class WishAdd(CreateView):
+    model = WishList
+    fields = '__all__'
+    success_url = '/'
